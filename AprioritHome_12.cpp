25502161ad4b913d331999e4de4735b7441b1757
Greshch <greshchook@gmail.com>
2022-07-13 09:24:10 +0300

    it was second task implemented

--- AprioritHome_12.cpp
+++ AprioritHome_12.cpp
@@ -11,9 +11,10 @@ struct Worker {
 
 int main()
 {
-    const int lenWorkers = 20;
+    int lenWorkers = 20;
     const int minSalary = 9000;
     const int maxSalary = 45000;
+    const int checkSalary = 9999;
     Worker* head = nullptr;
     Worker* tail = nullptr;
     // Fill start list and print
@@ -64,10 +65,50 @@ int main()
     {
         std::cout << "id : " << cur->id << "\t salary : " << cur->salary << "\n";
     }
+
+    // Task 2 add worker before typed id
+    std::cout << "\nInput id before : ";
+    int id = 0;
+    std::cin >> id;
+    Worker* prev = nullptr;
+
+    Worker* newWorker = new Worker;
+    ++lenWorkers;
+    newWorker->id = lenWorkers;
+    newWorker->salary = checkSalary;
+
+    Worker* workerId = head;
+    for (; workerId->id != id; workerId = workerId->next) // find needed worker by id
+    {
+        if (workerId->id == id)
+        {
+            break;
+        }
+        prev = workerId;
+    }
+
+    // add newWorker to list
+    //std::cout << "\nprev->id : " << prev->id << "\t salary : " << prev->salary << "\n";
+    //std::cout << "\nworkerId->id : " << workerId->id << "\t salary : " << workerId->salary << "\n";
+    newWorker->next = workerId;
+    if (prev == nullptr)
+    {
+        head = newWorker;
+    }
+    else
+    {
+        prev->next = newWorker;
+    }
     
+    // print list
+    std::cout << "\n\n";
+    for (Worker* cur = head; cur != nullptr; cur = cur->next)
+    {
+        std::cout << "id : " << cur->id << "\t salary : " << cur->salary << "\n";
+    }
 
     //delete list
-    Worker* prev = head;
+    prev = head;
     for (Worker* cur = head->next; cur != nullptr; cur = cur->next)
     {
         delete prev;
